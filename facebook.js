6281aeb2fedd0fc2f145d429cbee2f982d6ea2a7
Ignacio Céspedes <b.ignacio.cespedes@gmail.com>
2016-07-01 00:02:06 -0400

    Estilos Footer y comienzo de Section de bienvenida

---
+++ facebook.js
@@ -0,0 +1,17 @@
+(function(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1734800606758227',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.6&appId=1734800606758227";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+})
