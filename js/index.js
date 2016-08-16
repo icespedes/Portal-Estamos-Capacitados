c0d07fb45a03d297d1c57c9b857221d9a941ca8e
Ignacio Céspedes <b.ignacio.cespedes@gmail.com>
2016-08-15 21:50:10 -0300

    Se añade seccion de comentarios al detalle del servicio

--- js/index.js
+++ js/index.js
@@ -1,36 +1,33 @@
-$(document).ready(function(){
-
-	/*OPTION REGISTRO DE USUARIO*/
-	$('#opUserPesd').click(function() {
-		$('#selectInstitution').show();
-	});
-
-	$('#opUser').click(function() {
-		$('#selectInstitution').hide();
-	});
-
-  /*RATING STAR*/
-    $('.kv-ltr-theme-svg-star').rating({
-        hoverOnClear: false,
-        theme: 'krajee-svg',
-        displayOnly: true
-    });
-  
-  //$('.kv-fa-heart').on(
-  //  	'change', function () {
-  //	       console.log('Rating selected: ' + $(this).val());
-  //        });
-
-
-	/*******UTILIDADES BOOTSTRAP*******/
-	
-	/*BOOTSTRAP TOOLTIPS*/
-	$(function () {
-	  $('[data-toggle="tooltip"]').tooltip()
-	})
-
-	/*BOOTSTRAP POPOVER*/
-	$(document).ready(function(){
-	    $('[data-toggle="popover"]').popover();
-	});
+/*OPTION REGISTRO DE USUARIO*/
+$('#opUserPesd').click(function() {
+	$('#selectInstitution').show();
 });
+
+$('#opUser').click(function() {
+	$('#selectInstitution').hide();
+});
+
+/*RATING STAR*/
+$('.kv-ltr-theme-svg-star').rating({
+    hoverOnClear: false,
+    theme: 'krajee-svg',
+    displayOnly: true
+});
+
+//$('.kv-fa-heart').on(
+//  	'change', function () {
+//	       console.log('Rating selected: ' + $(this).val());
+//        });
+
+
+/*******UTILIDADES BOOTSTRAP*******/
+
+/*BOOTSTRAP TOOLTIPS*/
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+/*BOOTSTRAP POPOVER*/
+$(function () {
+	$("[data-toggle=popover]").popover();
+})
