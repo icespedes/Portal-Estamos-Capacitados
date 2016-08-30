0784346226c7d0fddc7e52949ed424d099476df3
Ignacio Céspedes <b.ignacio.cespedes@gmail.com>
2016-08-29 23:46:50 -0300

    Comienzo pagina Publicación

--- js/index.js
+++ js/index.js
@@ -14,6 +14,17 @@ $('.kv-ltr-theme-svg-star').rating({
     displayOnly: true
 });
 
+//CONTADOR DE CARACTERES DESCRIPCION SERVICIO
+var text_max = 300;
+$('#count_message').html(text_max + ' Caracteres');
+
+$('#service_description').keyup(function() {
+  var text_length = $('#service_description').val().length;
+  var text_remaining = text_max - text_length;
+  
+  $('#count_message').html(text_remaining + ' Caracteres');
+});
+
 //$('.kv-fa-heart').on(
 //  	'change', function () {
 //	       console.log('Rating selected: ' + $(this).val());
@@ -31,3 +42,9 @@ $(function () {
 $(function () {
 	$("[data-toggle=popover]").popover();
 })
+
+/* INPUT PHOTOS  http://plugins.krajee.com/file-input#features*/
+$("#input-fa").fileinput({
+    theme: "fa",
+    uploadUrl: "/uploadServicePhotos"
+});
